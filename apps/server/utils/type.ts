3932409bf2bb75c3d27f87fd16f6a8334cd34104
adheesh <adheeshzkp@gmail.com>
2025-12-29 00:36:52 +0530

    [BACKEND] | Created admin controllers

--- apps/server/utils/type.ts
+++ apps/server/utils/type.ts
@@ -1,58 +1,70 @@
 export interface CourseBody {
-  name: string;
-  description: string;
-  level: "BASIC" | "INTERMEDIATE" | "ADVANCE";
-  duration?: string;
-  instructorName: string;
+    name: string;
+    description: string;
+    level: "BASIC" | "INTERMEDIATE" | "ADVANCE";
+    duration?: string;
+    instructorName: string;
 }
 
 export interface UpdateCourse {
-  description: string;
-  level: "BASIC" | "INTERMEDIATE" | "ADVANCE";
-  duration?: string;
-  instructorName: string;
+    description: string;
+    level: "BASIC" | "INTERMEDIATE" | "ADVANCE";
+    duration?: string;
+    instructorName: string;
 }
 
 export interface CourseContentBody {
-  name: string;
-  description?: string;
-  sectionId: string;
-  videoUrl: string;
-  transcript?: string;
+    name: string;
+    description?: string;
+    sectionId: string;
+    videoUrl: string;
+    transcript?: string;
 }
 
 export interface updateCourseContent {
-  name?: string;
-  description?: string;
-  videoUrl?: string;
-  transcript?: string;
+    name?: string;
+    description?: string;
+    videoUrl?: string;
+    transcript?: string;
 }
 
 export interface CourseAssignmentBody {
-  name: string;
-  description: string;
-  instruction: string;
-  marksPerQuestion: string;
-  sectionId: string;
+    name: string;
+    description: string;
+    instruction: string;
+    marksPerQuestion: string;
+    sectionId: string;
 }
 
 export interface CourseAssignmentUpdate {
-  description?: string;
-  instruction?: string;
-  marksPerQuestion?: string;
+    description?: string;
+    instruction?: string;
+    marksPerQuestion?: string;
 }
 
 export interface AssignmentQuestionBody {
-  question: string;
-  options: string[];
-  correctAnswer: string[];
-  assignmentId: string;
-  type?: "MCQ" | "SCQ";
+    question: string;
+    options: string[];
+    correctAnswer: string[];
+    assignmentId: string;
+    type?: "MCQ" | "SCQ";
 }
 
 export interface UpdateAssignment {
-  question?: string;
-  options?: string[];
-  correctAnswer?: string[];
-  type?: "MCQ" | "SCQ";
+    question?: string;
+    options?: string[];
+    correctAnswer?: string[];
+    type?: "MCQ" | "SCQ";
+}
+
+export interface CreateAdminBody {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface UpdateAdminBody {
+    name?: string;
+    email?: string;
+    password?: string;
 }
